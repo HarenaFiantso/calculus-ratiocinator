e4771d25b41438a25c47067fd2e515f27e8edc9e
HarenaFiantso <fiantsorav@gmail.com>
2024-06-07 15:00:38 +0300

    test: verite

--- src/test/java/VeriteTest.java
+++ src/test/java/VeriteTest.java
@@ -1,3 +1,9 @@
+import com.projet1.affirmation.Verite;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class VeriteTest {
 
   @Test
