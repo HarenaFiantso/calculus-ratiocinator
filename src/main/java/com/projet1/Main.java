19332ca6b49787f3d3e32571c564a040e779381c
HarenaFiantso <fiantsorav@gmail.com>
2024-06-07 16:14:42 +0300

    chore: update main class test

--- src/main/java/com/projet1/Main.java
+++ src/main/java/com/projet1/Main.java
@@ -13,25 +13,25 @@ public class Main {
     Mensonge mensonge1 = new Mensonge("Lou est pauvre");
     AffirmationSimple affirmation1 = new AffirmationSimple("Lou est généreux", null);
 
-    /* Lou est pauvre et Lou est généreux: Doit retouner "null" */
+    /* Lou est pauvre et Lou est généreux: Doit retouner "faux" */
     Et exemple1 = new Et(mensonge1, affirmation1);
     System.out.println(exemple1.getDescription() + " : " + exemple1.getValeur());
 
-    /* Lou est beau. Donc Lou est pauvre: Doit retourner "false" */
+    /* Lou est beau. Donc Lou est pauvre: Doit retourner "faux" */
     Donc exemple2 = new Donc(verite1, mensonge1);
     System.out.println(exemple2.getDescription() + " : " + exemple2.getValeur());
 
-    /* Lou est pauvre. Donc Lou est généreux: Doit retourner "null" */
+    /* Lou est pauvre. Donc Lou est généreux: Doit retourner "jenesaispas" */
     Donc exemple3 = new Donc(verite1, affirmation1);
     System.out.println(exemple3.getDescription() + " : " + exemple3.getValeur());
 
-    /* Lou est beau ou Lou est généreux. Donc Lou est pauvre: Doit retourner "false" */
+    /* Lou est beau ou Lou est généreux. Donc Lou est pauvre: Doit retourner "faux" */
     Ou ouAffirmation = new Ou(verite1, affirmation1);
     Donc exemple4 = new Donc(ouAffirmation, mensonge1);
     System.out.println(exemple4.getDescription() + " : " + exemple4.getValeur());
 
     /* 1er : Lou est beau ou Lou est généreux. Donc Lou est pauvre */
-    /* 2e : Lou est pauvre ou Lou est généreux: Doit retourner "null" */
+    /* 2e : Lou est pauvre ou Lou est généreux: Doit retourner "faux" */
     Ou exemple5b = new Ou(mensonge1, affirmation1);
     Et exemple5 = new Et(exemple4, exemple5b);
     System.out.println(exemple5.getDescription() + " : " + exemple5.getValeur());
